6340e1ce3ac139ad01cdbea3e069c132a1ffb633
Pierre-O <sandozpierreolivier@yahoo.fr>
2020-05-26 18:16:40 +0200

    bad smell: switch statement, refactor: polymorhism replacing case with subclass

---
+++ src/main/java/gen_lab04_refactoring/ColorRed.java
@@ -0,0 +1,7 @@
+package gen_lab04_refactoring;
+
+public class ColorRed extends Color {
+    public String getColor(){
+        return "red";
+    }
+}
--- src/main/java/gen_lab04_refactoring/OrdersWriter.java
+++ src/main/java/gen_lab04_refactoring/OrdersWriter.java
@@ -11,7 +11,7 @@ public class OrdersWriter {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
-            
+
             sb.append("{");
             sb.append("\"id\": ");
             sb.append(getOrderForWriter(i).getOrderId());
